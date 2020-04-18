10973fd523e34d58b2409de47d9c3e6c518e5d5c
Puzzlemaker1 <bradfordthe3rd@gmail.com>
2020-04-17 23:28:19 -0400

    Grid system in place!

--- Assets/GridManager.cs
+++ Assets/GridManager.cs
@@ -6,15 +6,22 @@ public class GridManager : MonoBehaviour
 {
     public int xSize, ySize;
     public float xScale, yScale;
-    public GameObject prefab;
+    public Tile prefab;
+
+    Tile[,] tiles;
     // Start is called before the first frame update
     void Start()
     {
+        tiles = new Tile[xSize, ySize];
+        Tile clone;
         for(int x = 0; x < xSize; x++)
         {
             for(int y = 0; y < ySize; y++)
             {
-                Instantiate(prefab, new Vector3(xScale * x, yScale * y), Quaternion.identity);
+                clone = Instantiate(prefab, new Vector3(xScale * x, yScale * y), Quaternion.identity);
+                clone.yCoord = y;
+                clone.xCoord = x;
+                tiles[x, y] = clone;
             }
         }
     }
@@ -23,5 +30,10 @@ public class GridManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public Tile getTile(int x, int y)
+    {
+        return tiles[x, y];
     }
 }
---
+++ Assets/Tile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool blocking;
+    public int xCoord;
+    public int yCoord;
+
+    public enum TileTypeEnum
+    {
+        test1,
+        test2,
+        test3
+    }
+
+    TileTypeEnum type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        type = (TileTypeEnum)Random.Range(0, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
