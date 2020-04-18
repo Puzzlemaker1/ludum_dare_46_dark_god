87fcab7c2a6206a742575834017345df6a6f4f39
Puzzlemaker1 <bradfordthe3rd@gmail.com>
2020-04-17 23:00:27 -0400

    Adding a grid system to the game

---
+++ Assets/GridManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public int xSize, ySize;
+    public float xScale, yScale;
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int x = 0; x < xSize; x++)
+        {
+            for(int y = 0; y < ySize; y++)
+            {
+                Instantiate(prefab, new Vector3(xScale * x, yScale * y), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
