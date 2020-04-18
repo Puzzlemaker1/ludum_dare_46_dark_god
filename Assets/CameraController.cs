95fc5cdb97c26cf6b151dd8dc385ecb025a4d6b4
Puzzlemaker1 <bradfordthe3rd@gmail.com>
2020-04-18 11:54:37 -0400

    added camera system and moved some stuff around

---
+++ Assets/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveVec = new Vector3();
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            moveVec.y += moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveVec.x -= moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            moveVec.y -= moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            moveVec.x += moveSpeed;
+        }
+        this.transform.position += moveVec;
+    }
+}
