999a9381325b930fbe8b1c1b3b7bcb8fc1b1221f
Puzzlemaker1 <bradfordthe3rd@gmail.com>
2020-04-19 11:59:26 -0400

    Kngiht seeking working

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -31,24 +31,26 @@ public class Knight : BaseUnit
                 cultist.LoseHealth(1);
                 LoseHealth(1);
             }
-            else if(victim != null)
+            else if (victim != null)
             {
                 //Free the victims from their oppressors!
                 victim.LoseHealth(1);
             }
-            else if (!(tile is Castle))
+            else
             {
-                //We arn't on a castle, we should wander.
+                //No combat or victims to save.
                 List<Cultist> cultists = LocateGridEntity<Cultist>(1);
-                if(cultists.Count > 0)
+                if (cultists.Count > 0)
                 {
                     //WE FOUND A CULTIST, FUCK EM UP
                     //Grab one randomly and head towards it!
                     Debug.Log("FOUND A CULTIST, GET EM");
                     MoveUnit(cultists[Random.Range(0, cultists.Count)].GetComponentInParent<Tile>().coord - tile.coord);
                 }
-                else
+                else if (!(tile is Castle))
                 {
+                    //We arn't on a castle, we should wander.
+
                     //Move randomly
                     Vector2Int moveVec = new Vector2Int(Random.Range(-1, 2), Random.Range(-1, 2));
                     MoveUnit(moveVec);
@@ -56,9 +58,5 @@ public class Knight : BaseUnit
             }
             moveTimer = 0;
         }
-
-
-
-
     }
 }
