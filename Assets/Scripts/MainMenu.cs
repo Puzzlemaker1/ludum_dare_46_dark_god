ab8383f8e07d4df19db04d7ce8941644d18d3de9
D'Arcy <dmhamilt@ualberta.ca>
2020-04-18 18:03:12 -0600

    basic sound!

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class MainMenu : MonoBehaviour {
+
+
+	public Slider masterSlider;
+	public Slider effectsSlider;
+	public Slider ambienceSlider;
+	public Slider musicSlider;
+  public float volume;
+	public AudioSource[] SEaudioList;
+	public AudioSource[] AMBaudioList;
+	public AudioSource[] MUSaudioList;
+
+	void Start ()
+  {
+
+	}
+
+
+
+	public void MMUpdateSound ()
+	{
+		//GameObject.FindGameObjectWithTag ("GameController").GetComponent<AudioSource> ().volume = PlayerPrefsManager.GetMasterVolume () * PlayerPrefsManager.GetMusicVolume ();
+		//GameObject.Find ("WavesSounds").GetComponent<AudioSource> ().volume = PlayerPrefsManager.GetMasterVolume () * PlayerPrefsManager.GetAmbienceVolume ();
+		//GameObject.Find ("FireCrackleSounds").GetComponent<AudioSource> ().volume = PlayerPrefsManager.GetMasterVolume () * PlayerPrefsManager.GetSoundEffectVolume ();
+		//GameObject.Find ("Misc Sounds").GetComponent<AudioSource> ().volume = PlayerPrefsManager.GetMasterVolume () * PlayerPrefsManager.GetSoundEffectVolume ();
+
+
+		foreach (AudioSource source in SEaudioList) {
+			source.volume = masterSlider.value ;
+		}
+		foreach (AudioSource source in AMBaudioList) {
+			source.volume = masterSlider.value ;
+		}
+		foreach (AudioSource source in MUSaudioList) {
+			source.volume = masterSlider.value;
+		}
+
+	}
+
+
+
+	public void OnMasterSliderChange ()
+	{
+    volume = masterSlider.value;
+		MMUpdateSound ();
+	}
+	public void OnEffectsSliderChange ()
+	{
+		MMUpdateSound ();
+	}
+	public void OnMusicSliderChange ()
+	{
+		MMUpdateSound ();
+	}
+
+	public void QuitGame ()
+	{
+		Application.Quit ();
+	}
+}
